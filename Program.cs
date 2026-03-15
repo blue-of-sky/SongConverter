4bf27a3a1d58c5d7669bbc3603222058632dd726
blue-of-sky <aoiplay0141@gmail.com>
2026-03-15 17:52:28 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System.Text;
+
+namespace SongSorterApp;
+
+static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.SetHighDpiMode(HighDpiMode.SystemAware);
+        Application.Run(new MainForm());
+    }
+}
